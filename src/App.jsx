e889906f193f55c1ea8a2d98939cd159d512509b
mahmoud saad <mahmoudsaad51@gmail.com>
2025-11-05 19:14:59 +0200

    Prepare for deploy to GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,27 @@ import Contact from "./components/Contact/Contact";
 import Home from './components/Home/Home';
 import Layout from "./components/Layout/Layout";
 import Parent from "./components/Parent/Parent";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import NotFound from './components/NotFound/NotFound';
 import Gallery from './components/Gallary/Gallery';
 
-
-let _router = createBrowserRouter([
+let _router = createHashRouter([  
   {
     path: "/",
     element: <Layout />,
     children: [
-      { index : true, element: <Home /> },
+      { index: true, element: <Home /> },
       { path: "about", element: <About /> },
       { path: "contact", element: <Contact /> },
       { path: "parent", element: <Parent /> },
       { path: "gallery", element: <Gallery /> },
       { path: "*", element: <NotFound /> },
-
     ],
   },
 ]);
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={_router} />
-    </>
-  );
+  return <RouterProvider router={_router} />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
